7b7d531fe9e2cb943e0aeda6c596a756a5f97f29
Pietro Rampazzo <pietro@rampazzo.co>
2019-05-08 19:27:02 +0200

    eslint Dish.ts

--- src/Dish.ts
+++ src/Dish.ts
@@ -1,6 +1,6 @@
 import mongoose = require('mongoose');
 
-export interface Dish extends mongoose.Document{
+export interface Dish extends mongoose.Document {
     name: string,
     price: number,
     ingredients: string[]
@@ -21,19 +21,19 @@ var dishSchema = new mongoose.Schema({
     }
 });
 
-export function getSchema(){ return dishSchema; };
+export function getSchema() { return dishSchema; };
 
 var dishModel;
 
-export function getModel() : mongoose.Model< Dish >  { // Return Model as singleton
-    if( !dishModel ) {
-        dishModel = mongoose.model('Dish', getSchema() )
+export function getModel(): mongoose.Model<Dish> { // Return Model as singleton
+    if (!dishModel) {
+        dishModel = mongoose.model('Dish', getSchema())
     }
     return dishModel;
 }
 
-export function newDish( data ): Dish {
+export function newDish(data): Dish {
     var _dishmodel = getModel();
-    var dish = new _dishmodel( data );
+    var dish = new _dishmodel(data);
     return dish;
 }
