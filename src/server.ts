b4c42354ef2608c2109a8957f12990dc1e6e4dd6
Ettore <50149299+EttoreChinellato@users.noreply.github.com>
2019-06-04 14:43:34 +0200

    test

--- src/server.ts
+++ src/server.ts
@@ -213,12 +213,14 @@ app.put('/order', auth, (req, res, next) => {
                     }
                 }
                 o.save().then(() => {
-                    return res.status(200).json({ error: false, errormessage: "", id: o._id })
-                ;});
+                    return res.status(200).json({ error: false, errormessage: "", id: o._id });
+                });
             }).catch((reason) => {
                 return next({ statusCode: 404, error: true, errormessage: "DB error: " + reason });
             });
         }
+    }).catch((reason) => {
+        return next({ statusCode: 404, error: true, errormessage: "DB error: " + reason });
     });
 });
 
