1a3337d94714b1e5a468898a67cdf9fb2b173e66
MichelePessotto <38395793+MichelePessotto@users.noreply.github.com>
2019-06-06 14:39:04 +0200

    test

--- src/server.ts
+++ src/server.ts
@@ -182,6 +182,7 @@ app.put('/order', auth, (req, res, next) => {
                     else {
                         if (o.getFoodStatus() == 1) {
                             o.setDishReady(req.query.index);
+                            console.log(o.food_ready);
                             nsp_cashers.emit('dishCompleted');
                             if(o.orderCompleted()) {
                                 o.setFoodStatus();
